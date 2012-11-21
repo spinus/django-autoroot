ac76b5a6c3d9a91502be144e334de57f75b55865
Tomek Czyż <tomekczyz@gmail.com>
2012-11-21 01:22:22 +0100

    pep8

--- autoroot/management/__init__.py
+++ autoroot/management/__init__.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import User
 
 def create_superuser(app, created_models,**cos):
         import settings
-        if getattr(settings,'AUTOROOT_DEBUG_ONLY',True) and \
+        if getattr(settings, 'AUTOROOT_DEBUG_ONLY', True) and \
            not settings.DEBUG:
             return
-        su_name=getattr(settings,'AUTOROOT_USER','root')
-        su_email=getattr(settings,'AUTOROOT_EMAIL','root@example.com')
-        su_pass=getattr(settings,'AUTOROOT_PASSWORD','qqq')
+        su_name=getattr(settings, 'AUTOROOT_USER', 'root')
+        su_email=getattr(settings, 'AUTOROOT_EMAIL', 'root@example.com')
+        su_pass=getattr(settings, 'AUTOROOT_PASSWORD', 'qqq')
         try:
             User.objects.get(username=su_name)
         except User.DoesNotExist:
-            User.objects.create_superuser(su_name,su_email,su_pass)
-            print "Superuser created (%s:%s)"%(su_name,su_pass)
+            User.objects.create_superuser(su_name, su_email,su_pass)
+            print "Superuser created (%s:%s)"%(su_name, su_pass)
 
 post_syncdb.connect(create_superuser,sender=None)
 
